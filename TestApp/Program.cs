fdcd2807793de83c4d3122352f6fe7e5b5e3d429
Valentin Noves <vnoves@e-verse.com>
2023-01-30 19:35:27 -0300

    Add files via upload

---
+++ TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dracowrapper;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var decoder = new GltfDecoder();
+            var res = decoder.DecodeFromFileToScene(@"E:\\in.glb");
+            var scene = res.Value();
+            DracoCompressionOptions options = new DracoCompressionOptions();
+            SceneUtils.SetDracoCompressionOptions(options, scene);
+            var encoder = new GltfEncoder();
+            encoder.EncodeSceneToFile(scene, @"E:\\Test.gltf");
+        }
+    }
+}
